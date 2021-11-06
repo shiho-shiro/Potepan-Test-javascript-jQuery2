77c003265605e7f667c3de70e734536889a342fb
Shiho <seinoshiho@jouyashihonoMacBook-Air.local>
2021-11-06 23:10:33 +0900

    js課題２について

--- main.js
+++ main.js
@@ -38,14 +38,17 @@ function btNs(btN){
 				}
 		    }
 }
-function react(act){
-	let acTion = act;
-
-	if (display.innerHTML.slice(-1) == acTion) {
-		let ac = display.innerHTML.slice(0,-1)
-		    display.innerHTML = ac + acTion;
+function react(){
+	if (display.innerHTML.slice(-1) === "+") { //もし四則演算が一番右の"+"の場合
+		display.innerHTML = display.innerHTML.slice(0,-1) + "+"//dispayに表示されるのは、連続して出力される四則演算の最後の四則演算を引いた文字列
+	}else if (display.innerHTML.slice(-1) === "-") {
+		display.innerHTML = display.innerHTML.slice(0,-1) + "-"
+	}else if (display.innerHTML.slice(-1) === "*") {
+		display.innerHTML = display.innerHTML.slice(0,-1) + "*"
+	}else if (display.innerHTML.slice(-1) === "/") {
+		display.innerHTML = display.innerHTML.slice(0,-1) + "/"
 	}else{
-		 display.innerHTML += acTion.innerHTMLi;
+		 display.innerHTML += act.innerHTML; //上記に当てはまらな場合はdisplay　＝　displayに表示された値 + 四則演算
 	}
 }
 
