79301396a05253c6dcc8fb4eaca6739dc5ae2770
Shiho <seinoshiho@jouyashihonoMacBook-Air.local>
2021-11-07 14:24:57 +0900

    js.jquery課題2

--- main.js
+++ main.js
@@ -1,56 +1,45 @@
 let display = document.getElementById("display");
-let act = document.getElementsByClassName("act");
+
 
 function btNs(btN){
     let number = btN.innerHTML;
+
 		if (number == "AC") {
 			display.innerHTML = "0";
-			//eval()で計算。eval()はセキュリティリスクあり！
 		   }else if (number == "=") {
-              　display.innerHTML = eval(display.innerHTML);
+              　display.innerHTML = eval(display.innerHTML);//eval()で計算。eval()はセキュリティリスクあり！
 		    }else{
 				 if (display.innerHTML == "0") {
                      display.innerHTML  = number;
-				}
-				//上記同様"00"も先に表示されないようにする
-				else if(display.innerHTML == "00"){
+				}else if(display.innerHTML == "00"){//上記同様"00"も先に表示されないようにする
 					display.innerHTML = number;
-				}
-				//四則演算を先に表示されないようにする
-                else if(display.innerHTML == "+"){
-                    display.innerHTML =  number;
-                }
-				else if(display.innerHTML == "-"){
-                    display.innerHTML = number;
-                }
-                else if(display.innerHTML == "*"){
-                    display.innerHTML = number;
-                }
-                else if(display.innerHTML == "/"){
-                    display.innerHTML = number;
-                }
-                else if(display.innerHTML == "."){
-                    display.innerHTML = number;
-                }
-
-				//その他の数字がディスプレイに反映
-				else{
-					display.innerHTML += number;
+				}else{
+					display.innerHTML += number; //その他の数字がディスプレイに反映
 				}
 		    }
 }
-function react(){
-	if (display.innerHTML.slice(-1) === "+") { //もし四則演算が一番右の"+"の場合
-		display.innerHTML = display.innerHTML.slice(0,-1) + "+";//dispayに表示されるのは、連続して出力される四則演算の最後の四則演算を引いた文字列
-	}else if (display.innerHTML.slice(-1) === "-") {
-		display.innerHTML = display.innerHTML.slice(0,-1) + "-";
-	}else if (display.innerHTML.slice(-1) === "*") {
-		display.innerHTML = display.innerHTML.slice(0,-1) + "*";
-	}else if (display.innerHTML.slice(-1) === "/") {
-		display.innerHTML = display.innerHTML.slice(0,-1) + "/";
-	}else{
-		 display.innerHTML += act.innerHTML; //上記に当てはまらな場合はdisplay　＝　displayに表示された値 + 四則演算
-	}
+
+$(function(){
+	let bt = $("button");
+        bt.click(function(){
+	    $(bt).removeClass("button-color");//無職状態にして
+	    $(this).addClass("button-color");//枠の青色を入れる
+    });
+});//ボタンが押される度にイベント発火
+
+
+function acts(act){
+  if (display.innerHTML.slice(-1) === "+"){
+       return;
+   }else if (display.innerHTML.slice(-1) === "-") {
+       return;
+   }else if (display.innerHTML.slice(-1) === "*") {
+	   return;
+   }else if (display.innerHTML.slice(-1) === "/") {
+	   return;
+   }else{
+	   display.innerHTML += act.innerHTML;
+    }
 }
 
 
